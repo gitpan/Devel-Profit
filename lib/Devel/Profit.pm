65c6fb145447b163ab8bc0313db37ebab5d212f4
Léon Brocard <acme@astray.com>
2008-10-16 19:34:48 +0000

    Import of LBROCARD/Devel-Profit-0.34 from CPAN.
    gitpan-cpan-distribution: Devel-Profit
    gitpan-cpan-version:      0.34
    gitpan-cpan-path:         LBROCARD/Devel-Profit-0.34.tar.gz
    gitpan-cpan-author:       LBROCARD
    gitpan-cpan-maturity:     released

--- lib/Devel/Profit.pm
+++ lib/Devel/Profit.pm
@@ -1,7 +1,7 @@
 package Devel::Profit;
 use strict;
 use warnings;
-our $VERSION = '0.33';
+our $VERSION = '0.34';
 
 use DynaLoader ();
 our @ISA = qw(DynaLoader);
@@ -9,3 +9,20 @@ our @ISA = qw(DynaLoader);
 bootstrap Devel::Profit $VERSION;
 
 1;
+
+=head1 NAME
+
+Devel::Profit - A Perl profiler
+
+=head1 AUTHOR
+
+Leon Brocard, C<< <acme@astray.com> >>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2008, Leon Brocard
+
+=head1 LICENSE
+
+This module is free software; you can redistribute it or modify it
+under the same terms as Perl itself.
